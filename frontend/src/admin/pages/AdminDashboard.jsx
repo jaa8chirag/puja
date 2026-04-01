042c2facf3951a275b73587d10be7446d49fddd6
Chirag Kumar <chiragchoudhary340@gmail.com>
2026-04-01 13:34:43 +0530

    add admin

--- frontend/src/admin/pages/AdminDashboard.jsx
+++ frontend/src/admin/pages/AdminDashboard.jsx
@@ -101,6 +101,23 @@ const Icon = ({ name }) => {
         <line x1="6" y1="6" x2="18" y2="18" />
       </svg>
     ),
+    gallery: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
+        <circle cx="9" cy="9" r="2" />
+        <path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21" />
+      </svg>
+    )
   };
   return icons[name] || null;
 };
@@ -119,6 +136,7 @@ const Sidebar = ({ active, setActive, isOpen, onClose }) => {
     { label: "Analytics", icon: "analytics" },
     { label: "Blogs", icon: "bookings" },
     { label: "Pages", icon: "services" },
+    { label: "Gallery", icon: "gallery" },
   ];
   return (
     <>
@@ -301,6 +319,8 @@ const AdminDashboard = () => {
             {active === "Analytics" && <AnalyticsDashboard />}
             {active === "Blogs" && <Adminblogs />}
             {active === "Pages" && <AdminPages />}
+            {active === "Gallery" && <Gallery />}
+
           </div>
         </div>
       </div>
